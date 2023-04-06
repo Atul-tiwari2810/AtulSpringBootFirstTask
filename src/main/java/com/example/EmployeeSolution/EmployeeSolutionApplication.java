baa539034329a0c2c8672d99b7ec66e6997a2e26
atultiwari <atul@wowfinstack.com>
2023-04-06 06:39:15 +0100

    first commit

---
+++ src/main/java/com/example/EmployeeSolution/EmployeeSolutionApplication.java
@@ -0,0 +1,13 @@
+package com.example.EmployeeSolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeSolutionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmployeeSolutionApplication.class, args);
+	}
+
+}
